7dea340099b24a4f7725e05cbbfd34ffe349fdf7
Kaarti22 <kaartimondi@gmail.com>
2025-05-28 16:30:42 +0530

    update video tracker using lines drawn

--- track_video.py
+++ track_video.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 import json
 from detector import YOLOv11Detector
 from tracker import Tracker
@@ -8,34 +7,40 @@ from logger_config import setup_logger
 
 logger = setup_logger()
 
-VIDEO_PATH = "sample.mp4"
-LINE1_Y = 415
-LINE2_Y = 450
-REAL_DISTANCE_M = 10.0
+line_config_path = "gui/line_config.json"
+with open(line_config_path, "r") as f:
+    config = json.load(f)
 
+line1_pts = config["line1"]
+line2_pts = config["line2"]
+real_distance_m = config["real_world_distance_m"]
+
+line1_y = int((line1_pts[0][1] + line1_pts[1][1]) / 2)
+line2_y = int((line2_pts[0][1] + line2_pts[1][1]) / 2)
+pixel_distance = abs(line2_y - line1_y)
+
+VIDEO_PATH = "sample.mp4"
 cap = cv2.VideoCapture(VIDEO_PATH)
+
 fps = cap.get(cv2.CAP_PROP_FPS)
 width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-pixel_distance = abs(LINE2_Y - LINE1_Y)
-
-meters_per_pixel = REAL_DISTANCE_M / pixel_distance
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter("result.mp4", fourcc, fps, (width, height))
 
 detector = YOLOv11Detector()
 tracker  = Tracker()
-speed_est = SpeedEstimator(real_distance_m=REAL_DISTANCE_M)
+speed_est = SpeedEstimator(real_distance_m=real_distance_m)
 
 speed_log = []
 logged_ids = set()
 
 frame_idx = 0
 
-while True:
-    logger.info("Video processing started.")
+logger.info("Video processing started.")
 
+while True:
     ret, frame = cap.read()
     if not ret:
         logger.info("End of video reached or failed to read frame.")
@@ -58,7 +63,7 @@ while True:
         y_center = (y1 + y2) // 2
 
         speed_kmph = speed_est.update_and_get_speed(
-            track_id, y_center, timestamp, LINE1_Y, LINE2_Y
+            track_id, y_center, timestamp, line1_y, line2_y
         )
 
         if speed_kmph is not None and track_id not in logged_ids:
@@ -75,14 +80,13 @@ while True:
 
         if tr.speed is not None and tr.speed >= 5:
             label = f"{tr.speed:.1f} km/h"
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,0), 2)
-            cv2.putText(frame, label, ((x1 + x2) // 2 - 30, (y1 + y2) // 2), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,255), 2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(frame, label, ((x1 + x2) // 2 - 30, (y1 + y2) // 2), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
 
-    cv2.line(frame, (0, LINE1_Y), (width, LINE1_Y), (255,0,0), 2)
-    cv2.line(frame, (0, LINE2_Y), (width, LINE2_Y), (0,0,255), 2)
+    cv2.line(frame, tuple(map(int, line1_pts[0])), tuple(map(int, line1_pts[1])), (255, 0, 0), 2)
+    cv2.line(frame, tuple(map(int, line2_pts[0])), tuple(map(int, line2_pts[1])), (0, 0, 255), 2)
 
     cv2.imshow("Vehicles Speed Detection", frame)
-
     out.write(frame)
 
     if cv2.waitKey(1) == ord('q'):
